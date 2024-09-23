816fd18e18ecf52d6aed7243daef3599e9898665
doras <doras@rapyd.net>
2024-09-23 16:35:47 +0300

    dictionary.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import justpy as jp
+
+from webapp.dictionary import Dictionary
 from webapp.home import Home
 from webapp.about import About
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
+
 
 jp.justpy(port=8001)
