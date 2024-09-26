13b0743114a2c44629efb9a02a3c562c35302cad
doras <doras@rapyd.net>
2024-09-26 08:20:43 +0300

    dictionary with instant definition

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,21 +1,43 @@
 import justpy as jp
+from definition import Definition
 
 
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instantly as you type", classes="text-lg")
 
-        jp.Input(a=div, placeholder="Enter an English word",
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+
+        input_box = jp.Input(a=input_div, placeholder="Enter an English word",
                  classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:bg-white focus:outline-none focus:border-purple-500 "
-                         "py-2 px-4 ")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40 border-gray-400")
+                         "py-2 px-4 ", outputdiv=output_div)
+        input_box.on("input", cls.get_definition)
+        # jp.Button(a=input_div, text="Get Definition", classes="border-2 border-gray-300 text-gray-500", click=cls.get_definition,
+        #           outputdiv=output_div, inputbox=input_box)
 
         return wp
+
+    # get_definition with button with input box
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = Definition(widget.value).get()
+        widget.outputdiv.text = " ".join(defined)
+
+    # get_definition with button
+    # @staticmethod
+    # def get_definition(widget, msg):
+    #     defined = Definition(widget.inputbox.value).get()
+    #     widget.outputdiv.text = " ".join(defined)
+
+
+
 
 
 
